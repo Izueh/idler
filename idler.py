7457089e3ca90e59f9a522fc2cb75bea7bea55f8
Isaac Ramos <bldkaiser@gmail.com>
2017-08-01 00:44:55 -0400

    sample endpoints

--- idler.py
+++ idler.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from model.action import Attack
 
-app = Flask()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+# way #1 to do things
+@app.route('/action/attack', methods=['POST'])
+def attack():
+    json = request.get_json()
+    # handle request
+    # make response
+    return jsonify(response)
+
+#way #2  more modular
+
+app.add_url_rul('/action/attack2',view_func=Attack.as_view('attack',methods=['POST']))
+
+
+    
 
 
 if __name__ == '__main__':
     app.run()
+
